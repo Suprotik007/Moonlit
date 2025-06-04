5953d6873c3ed62679d8dc34c72ac7ddb5239c57
Suprotik007 <csuprotik@gmail.com>
2025-06-04 22:45:58 +0600

    added animation to login and reg page , created error page for wrong routes(fixed commit)

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,22 +1,22 @@
 
-// import React from 'react';
-// import Lottie from "lottie-react";
-// import { Link } from 'react-router';
-// import errorLottie from '../assets/errorLottie.json'
-// const ErrorPage = () => {
-//   return (
-//     <div>
+import React from 'react';
+import Lottie from "lottie-react";
+import { Link } from 'react-router';
+import errorLottie from '../assets/errorLottie.json'
+const ErrorPage = () => {
+  return (
+    <div>
       
-//       <div className='mx-4 sm:mx-8 md:mx-20 lg:mx-80    text-center'>
-//            <Lottie className='' animationData={errorLottie} loop={true}> </Lottie>
-//         <Link to='/'>
-//           <button className="btn  btn-outline border-3 text-gray-700 rounded-4xl hover:bg-black hover:text-white  btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
-//             Go Home
-//           </button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className='mx-4 sm:mx-8 md:mx-20 lg:mx-80    text-center'>
+           <Lottie className='' animationData={errorLottie} loop={true}> </Lottie>
+        <Link to='/'>
+          <button className="btn  btn-outline border-3 text-gray-700 rounded-4xl hover:bg-black hover:text-white  btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+            Go Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-// export default ErrorPage;
+export default ErrorPage;
