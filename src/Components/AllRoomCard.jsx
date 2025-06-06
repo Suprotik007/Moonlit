a432bca553fbd8b34012a8952d50d5f06b815fb7
Suprotik007 <csuprotik@gmail.com>
2025-06-06 16:07:52 +0600

    created allRooms section and displayed allRooms from db

---
+++ src/Components/AllRoomCard.jsx
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import { Link } from 'react-router';
+import { motion } from "motion/react"
+
+const AllRoomCard = ({ singleRoom }) => {
+  return (
+   <Link to='/roomDetails'>
+    <motion.div
+      className="rounded-2xl text-left bg-base-200 m-5 border-2 border-gray-700 shadow-lg"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{rotate:360,  opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      whileHover={{ scale: 1.10, boxShadow: "0px 10px 20px rgba(0,0,0,0.2)" }}
+    >
+      <figure>
+        <img className="rounded-2xl p-1 " src={singleRoom.image} />
+      </figure>
+      <div className="p-5">
+        <h2 className="card-title text-2xl">{singleRoom.title}</h2> 
+
+        <p className="text-gray-700">{singleRoom.short_description}</p> <br />
+        <p className="font-semibold">
+          Price : <span className="badge  border-2 border-gray-700 badge-outline">{singleRoom.price}</span>
+        </p> 
+        
+      </div>
+    </motion.div>
+   </Link>
+  );
+};
+
+export default AllRoomCard;
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllRoomCard from '../Components/AllRoomCard';
 
 const Rooms = () => {
+    const [allRooms,setAllRooms]=useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/allRooms')
+        .then(res=>res.json())
+        .then(data=>{setAllRooms(data);
+        })
+    })
     return (
         <div>
-            rooms
+        
+             <h1 className='text-4xl text-center font-semibold text-gray-600 mt-15 border-b-2 pb-5 w-4/12 mx-auto mb-15'>Choose Your Cozy room</h1>
+
+             <div  className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 mx-auto pt-8'>
+{
+    allRooms.map(singleRoom=>(<AllRoomCard key={singleRoom.id} singleRoom={singleRoom}></AllRoomCard>))
+}
+             </div>
         </div>
     );
 };
